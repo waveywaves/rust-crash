ea1a5b53c50f3d8ecd5f71fd5083025f77deb9d0
Vibhav Bobade <vibhav.bobde@gmail.com>
2022-10-12 12:51:16 +0530

    completed Banas's tutorial till shadowing

--- src/main.rs
+++ src/main.rs
@@ -11,10 +11,22 @@ fn main() {
     println!("What is your name ?");
 
     let mut name = String::new();
+    let mut age: String = String::new();
     let greeting = "Nice to meet you !";
 
     io::stdin().read_line(&mut name)
         .expect("Didn't Receive Input");
 
-    println!("Hello, {}! {}", name.trim_end(), greeting);
+    println!("What is your age ?");
+
+    io::stdin().read_line(&mut age)
+        .expect("Didn't Receive Input");
+
+    // Shadow age
+    let mut age: u32 = age.trim().parse()
+        .expect("Age wasn't assigned a number");
+
+    age = age + 1;
+
+    println!("Hello, {}! {}! You {} - 1 year old badass !", name.trim_end(), greeting, age);
 }
