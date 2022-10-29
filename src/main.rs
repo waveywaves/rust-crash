53fe406f4e89e55c6f10595802be74f1fd93bad7
Vibhav Bobade <vibhav.bobde@gmail.com>
2022-10-29 18:50:29 +0530

    heap, smart pointers, thread

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,10 @@ use std::fs::File;
 use std::cmp::Ordering;
 use std::ops::Add;
 use std::collections::HashMap;
+use std::thread;
+use std::time::Duration;
+use std::rc::Rc;
+use std::sync::{Arc, Mutex};
 
 mod restaurant;
 use crate::restaurant::order_food;
@@ -354,4 +358,95 @@ fn main() {
 
     println!("from sum closure : {}",use_fn(1, 2, sum));
     println!("from prod closure : {}",use_fn(1, 2, prod));
+
+    // Allocating on the Heap
+
+    let b_int1 = Box::new(10);
+    println!("box int {b_int1}");
+
+    struct TreeNode<T> {
+        pub left: Option<Box<TreeNode<T>>>,
+        pub right: Option<Box<TreeNode<T>>>,
+        pub key: T,
+    };
+    
+    impl<T> TreeNode<T> {
+        pub fn new(key: T) -> Self {
+            TreeNode {
+                left:None,
+                right: None,
+                key,
+            }
+        }
+        pub fn left(mut self, node: TreeNode<T>) -> 
+        Self{
+            self.left = Some(Box::new(node));
+            self
+        }
+        pub fn right(mut self, node: TreeNode<T>) -> 
+        Self{
+            self.right = Some(Box::new(node));
+            self
+        }
+    }
+
+    let node1 = TreeNode::new(1)
+    .left(TreeNode::new(2))
+    .right(TreeNode::new(3));
+
+    // Thread
+
+    let thread1 = thread::spawn(|| {
+        for i in 1..25 {
+            println!("Spawned thread : {}", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    for i in 1..20 {
+        println!("Main thread: {}", i);
+        thread::sleep(Duration::from_millis(1));
+    }
+
+    thread1.join().unwrap();
+
+    pub struct Bank {
+        balance: f32
+    }
+
+    // Smart Pointers
+
+    fn withdraw(the_bank: &Arc<Mutex<Bank>>, amt: f32) {
+        let mut bank_ref = the_bank.lock().unwrap();
+        if bank_ref.balance < 5.0 {
+            println!("Current Balance : {} Withdrawal a smaller amount", bank_ref.balance);
+        } else {
+            bank_ref.balance -= amt;
+            println!("Customer withdrew {} Current Balance {}", amt, bank_ref.balance)
+        }
+    }
+    
+    fn customer(the_bank: &Arc<Mutex<Bank>>) {
+        withdraw(&the_bank, 5.0);
+    }
+
+    let bank: Arc<Mutex<Bank>> = Arc::new(Mutex::new(
+        Bank{balance: 100.0}
+    ));
+
+    let handles = (0..10).map(
+        |_| {
+            let bank_ref = bank.clone();
+            thread::spawn( move || {
+                customer(&bank_ref);
+            })
+        }
+    );
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
+
+    println!("Total {}", bank.lock().unwrap().balance);
+
 }
